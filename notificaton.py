2454ffff1841e39a785336137fcf95be93664414
chauhanhritik <chauhanghritik@gmail.com>
2021-05-09 19:21:17 +0530

    -

---
+++ notificaton.py
@@ -0,0 +1,7 @@
+from plyer import notification 
+notification.notify(
+    title = "this will be the title. ",
+    message = "type your message here. ",
+    #  app_icon = "C:\Users\NSC\Downloads\clock.ico",
+    timeout = 4
+          )
