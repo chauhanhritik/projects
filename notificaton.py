33c811e99a693160719e6d2c340623b04884fc49
chauhanhritik <chauhanghritik@gmail.com>
2021-05-09 19:37:07 +0530

    added sleep time

--- notificaton.py
+++ notificaton.py
@@ -1,7 +1,11 @@
 from plyer import notification 
-notification.notify(
-    title = "this will be the title. ",
-    message = "type your message here. ",
-    #  app_icon = "C:\Users\NSC\Downloads\clock.ico",
-    timeout = 4
-                   ) 
+import time
+while True:
+
+    notification.notify(
+        title = "this will be the title. ",
+        message = "type your message here. ",
+        # app_icon = "C:\Users\NSC\Downloads\clock.ico"
+        timeout = 4
+                       ) 
+    time.sleep(10)
